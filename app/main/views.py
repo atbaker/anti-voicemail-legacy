e63e851632cc7db2e9439b519a74d5142abcc47d
Andrew T. Baker <abaker@twilio.com>
2015-10-03 13:34:02 -0400

    AND SO IT BEGINS

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,42 @@
+from flask import url_for, request
+from twilio import twiml
+
 from . import main
 
 
 @main.route('/')
 def index():
     return 'foo'
+
+@main.route('/voicemail', methods=['POST'])
+def record_voicemail():
+    """Record a new voicemail"""
+    resp = twiml.Response()
+
+    # Tell the caller they reached the voicemail
+    resp.say('Andrew Baker is unable to answer the phone. Please leave a \
+              message after the beep.')
+
+    # Record and transcribe their message
+    resp.record(action=url_for('main.hang_up'), transcribe=True,
+                transcribeCallback=url_for('main.send_notification'))
+
+    return str(resp)
+
+@main.route('/hang-up', methods=['POST'])
+def hang_up():
+    """
+    Ends a call. Only used when a caller has been droning on for more than
+    two minutes.
+    """
+    resp = twiml.Response()
+
+    resp.say('Your message has been recorded. Goodbye.')
+    resp.hangup()
+
+    return str(resp)
+
+@main.route('/send-notification', methods=['POST'])
+def send_notification():
+    """Receives a transcribed voicemail from Twilio and sends a notification"""
+    import pdb; pdb.set_trace()
